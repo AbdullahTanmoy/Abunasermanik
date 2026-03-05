361b2fbccd223f258241e2cc89364df5d9d70eba
Abdullah Tanmoy <tanmoyabdullah@gmail.com>
2026-03-05 14:54:06 +0600

    2nd commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
       fontFamily: {
         inter: ["Inter", "sans-serif"],
         heebo: ["Heebo", "sans-serif"],
       },
     },
-    },
-    plugins: [],
-  }
+  },
+  plugins: [],
+}
